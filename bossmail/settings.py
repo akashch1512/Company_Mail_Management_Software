5073436b89534c2e446a7c05bc4e0fc727766fa2
akashch1512 <akashchaudhari1012@gmail.com>
2025-11-06 23:42:39 +0530

    Database Added

--- bossmail/settings.py
+++ bossmail/settings.py
@@ -1,5 +1,8 @@
 from pathlib import Path
 import os
+import dj_database_url
+from dotenv import load_dotenv
+load_dotenv()
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -48,13 +51,13 @@ TEMPLATES = [
 WSGI_APPLICATION = "bossmail.wsgi.application"
 
 
-# Dev DB: SQLite. Swap to Postgres later.
+
+
 DATABASES = {
-"default": {
-"ENGINE": "django.db.backends.sqlite3",
-"NAME": BASE_DIR / "db.sqlite3",
+    "default": dj_database_url.parse(os.environ.get("database_url"), conn_max_age=600, ssl_require=True,
+    )
 }
-}
+
 
 
 STATIC_URL = "/static/"
